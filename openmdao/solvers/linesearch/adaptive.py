bf4e0fc716877c42d617309475ce03d5083f2ad0
lamkina <lamkin@umich.edu>
2022-01-25 17:33:21 -0500

    Added infrastructure for backtracking and forward tracking, but waiting on Eytan to implement the forward tracking algorithm

--- openmdao/solvers/linesearch/adaptive.py
+++ openmdao/solvers/linesearch/adaptive.py
@@ -9,6 +9,8 @@ AdaptiveLS -- Enforces bounds using the vector method.
 # TODO: Add the forward tracking logic and algorithm
 # TODO: Make sure alpha is properly set as a class attribute so that it can be used in the Newton solver class.
 
+import warnings
+
 import numpy as np
 
 from openmdao.core.analysis_error import AnalysisError
@@ -90,33 +92,45 @@ class AdaptiveLS(LinesearchSolver):
         float
             Line search objective (residual norm).
         """
+        # TODO: Need a better way to handle the penalty_residual check
+        # to ensure the user doesn't attempt to use that option with
+        # an incorrect solver type.
 
         # Compute the penalized residual norm
         if self.options["penalty_residual"]:
-            system = self._system()
-            u = system._outputs.asarray()
-            resids = system._residuals.asarray()
-            lb = self._lower_bounds
-            ub = self._upper_bounds
-            lb_mask = self._lower_finite_mask
-            ub_mask = self._upper_finite_mask
+            if self._lower_finite_mask is not None and self._upper_finite_mask is not None:
+                if self.mu_lower is not None and self.mu_upper is not None:
+                    system = self._system()
+                    u = system._outputs.asarray()
+                    resids = system._residuals.asarray()
+                    lb = self._lower_bounds
+                    ub = self._upper_bounds
+                    lb_mask = self._lower_finite_mask
+                    ub_mask = self._upper_finite_mask
 
-            penalty = np.zeros(u.size)
+                    penalty = np.zeros(u.size)
 
-            t_lower = u[lb_mask] - lb[lb_mask]
-            t_upper = ub[ub_mask] - u[ub_mask]
+                    t_lower = u[lb_mask] - lb[lb_mask]
+                    t_upper = ub[ub_mask] - u[ub_mask]
 
-            if t_lower.size > 0:
-                penalty[lb_mask] += np.sum(self.mu_lower * -np.log(t_lower + 1e-10))
+                    if t_lower.size > 0:
+                        penalty[lb_mask] += np.sum(self.mu_lower * -np.log(t_lower + 1e-10))
 
-            if t_upper.size > 0:
-                penalty[ub_mask] += np.sum(self.mu_upper * -np.log(t_upper + 1e-10))
+                    if t_upper.size > 0:
+                        penalty[ub_mask] += np.sum(self.mu_upper * -np.log(t_upper + 1e-10))
 
-            return np.linalg.norm(resids + penalty)
+                    return np.linalg.norm(resids + penalty)
+                else:
+                    raise warnings.warn(
+                        "Cannot use the penalty residual options without the interior penalty solver.  Defaulting to the unpenalized residual."
+                    )
+            else:
+                raise warnings.warn(
+                    "Cannot use the penalty residual options without the interior penalty solver.  Defaulting to the unpenalized residual."
+                )
 
         # Compute the unpenalized residual norm
-        else:
-            return self._iter_get_norm()
+        return self._iter_get_norm()
 
     def _iter_initialize(self):
         """
@@ -131,6 +145,7 @@ class AdaptiveLS(LinesearchSolver):
         """
         system = self._system()
         self.alpha = alpha = self.options["alpha"]
+        use_fwd_track = False
 
         u = system._outputs
         du = system._vectors["output"]["linear"]
@@ -154,6 +169,8 @@ class AdaptiveLS(LinesearchSolver):
 
             self._run_apply()
             phi = self._line_search_objective()
+            if phi < phi0:
+                use_fwd_track = True
 
         except AnalysisError as err:
             self._solver_info.restore_cache(cache)
@@ -165,7 +182,7 @@ class AdaptiveLS(LinesearchSolver):
 
             phi = np.nan
 
-        return phi
+        return phi, use_fwd_track
 
     def _single_iteration(self):
         """
@@ -227,42 +244,35 @@ class AdaptiveLS(LinesearchSolver):
         elif method == "goldstein":
             return fval0 + (1 - c1) * alpha * df_dalpha <= fval <= fval0 + c1 * alpha * df_dalpha
 
-    def _update_step_length_parameter(self, rho):
+    def _update_step_length_parameter(self, bt_factor):
         """
         Update the step length parameter by multiplying with the contraction factor.
 
         Parameters
         ----------
-        rho : float
+        bt_factor : float
             Contraction factor
         """
-        self.alpha *= rho  # update alpha
+        self.alpha *= bt_factor  # update alpha
 
-    def _solve(self):
-        """
-        Run the iterative solver.
-        """
+    def _backtracking(self, phi0):
         options = self.options
         maxiter = options["maxiter"]
-        rho = options["rho"]
+        bt_factor = options["bt_factor"]
         method = options["method"]
 
         system = self._system()
         u = system._outputs
         du = system._vectors["output"]["linear"]  # Newton step
 
-        self._iter_count = 0
-        phi = self._iter_initialize()
-        phi0 = self._phi0
-
         # Further backtracking if needed.
         while self._iter_count < maxiter and (not self._stopping_criteria(phi, method) or self._analysis_error_raised):
 
-            with Recording("ArmijoGoldsteinLS", self._iter_count, self) as rec:
+            with Recording("AdaptiveLS", self._iter_count, self) as rec:
 
                 if self._iter_count > 0:
                     alpha_old = self.alpha
-                    self._update_step_length_parameter(rho)
+                    self._update_step_length_parameter(bt_factor)
                     # Moving on the line search with the difference of the old and new step length.
                     u.add_scal_vec(self.alpha - alpha_old, du)
                 cache = self._solver_info.save_cache()
@@ -291,6 +301,81 @@ class AdaptiveLS(LinesearchSolver):
 
             # self._mpi_print(self._iter_count, norm, norm / norm0)
             self._mpi_print(self._iter_count, phi, self.alpha)
+
+    def _quad_interp_min(self, x, f):
+        x_min = 0.5 * (x[0] + x[2]) + 0.5 * ((f[0] - f[1]) * (f[1] - f[2]) * (f[3] - f[0])) / (
+            (x[1] - x[2]) * f[0] + (x[2] - x[0]) * f[1] + (x[0] - x[1]) * f[0]
+        )
+        return x_min
+
+    def _forwardtracking(self, phi):
+        # TODO: Add print statements after each model evaultion
+        # TODO: Clean up and refactor this function
+        # TODO: Add ability to run more interpolation iterations to better converge on the optimal alpha
+        options = self.options
+        ft_factor = options["ft_factor"]
+        alpha_max = options["alpha_max"]
+
+        system = self._system()
+        u = system._outputs
+        du = system._vectors["output"]["linear"]  # Newton step
+
+        # We want to store the last three alpha and phi values to do
+        # a quadratic interpolation for the best alpha value
+        alpha_hist = []
+        phi_hist = []
+
+        # First, we need to make sure alpha_max doesn't violate a bound.
+        # If alpha_max violates a bound, we need to set it to a value that will cut
+        # off right at the bound.
+        du_arr = du.asarray()
+        u_arr = u.asarray()
+        u_max = u_arr + alpha_max * du_arr
+
+        # This is the required change in step size, relative to the du vector.
+        d_alpha = 0
+
+        # Find the largest amount a bound is violated
+        # where positive means a bound is violated - i.e. the required d_alpha.
+        du_arr = du.asarray()
+        mask = du_arr != 0
+        if mask.any():
+            abs_du_mask = np.abs(du[mask])
+            u_mask = u_max[mask]
+
+            # Check lower bound
+            if self._lower_bounds is not None:
+                max_d_alpha = np.amax((self._lower_bounds[mask] - u_mask) / abs_du_mask)
+                if max_d_alpha > d_alpha:
+                    d_alpha = max_d_alpha
+
+            # Check upper bound
+            if self._upper_bounds is not None:
+                max_d_alpha = np.amax((u_mask - self._upper_bounds[mask]) / abs_du_mask)
+                if max_d_alpha > d_alpha:
+                    d_alpha = max_d_alpha
+
+        # Adjust alpha_max so that it goes right to the most restrictive
+        # bound.
+        if d_alpha > 0:
+            alpha_max = alpha_max - d_alpha
+
+        # TODO: Add bracketing algorithm
+        # TODO: Add interpolation logic
+
+    def _solve(self):
+        """
+        Run the iterative solver.
+        """
+
+        self._iter_count = 0
+        phi, use_fwd_track = self._iter_initialize()
+        phi0 = self._phi0
+
+        if use_fwd_track:
+            self._forwardtracking(phi)
+        else:
+            self._backtracking(phi0)
 
 
 def _enforce_bounds_vector(u, du, alpha, lower_bounds, upper_bounds):
